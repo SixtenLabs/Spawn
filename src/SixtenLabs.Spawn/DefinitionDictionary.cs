51b302542814353a39828bb589cc0b2df8b71ba0
Patrick Glaspey <pglaspey@gmail.com>
2016-10-06 17:08:03 -0500

    Added interface to DefinitionDictionary

--- src/SixtenLabs.Spawn/DefinitionDictionary.cs
+++ src/SixtenLabs.Spawn/DefinitionDictionary.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SixtenLabs.Spawn
 {
-  public class DefinitionDictionary
+  public class DefinitionDictionary : IDefinitionDictionary
   {
     public string GetTranslatedName(string specName)
     {
